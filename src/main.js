b12050402f9726f3bc52b177c4c4b0891d6971d0
GreatVincy <1358073296@qq.com>
2018-04-10 17:27:05 +0800

    配置sass, element, normalize.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
+import AppStyle from './App.sass'
 import router from './router'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
