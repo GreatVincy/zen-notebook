272414fae1d98bb92d0cc8742f3a2ff3821bfe2d
GreatVincy <1358073296@qq.com>
2018-04-16 06:51:32 +0800

    编辑器页面布局设计与实现(Flex&Scss&Pug)

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,40 +1,134 @@
 <template lang="pug">
 div.editor-root
-  div.editor-tools-bar
   div.editor-container
     div.editor-input
+      div.editor-tools-bar
+        div.editor-tools-bar-title
+          input.editor-title-input(type="text" placeholder="请输入标题")
+        div.editor-tools-bar-icons
+          i.el-icon-picture(title="插入图片")
+          i.el-icon-arrow-left(title="撤销")
+          i.el-icon-arrow-right(title="重做")
+          i.el-icon-document(title="保存")
+          i.el-icon-edit(title="预览/编辑")
+          i.el-icon-success(title="发布")
+      div.editor-textarea
+        textarea
     div.editor-preview
+      div.editor-preview-title TestTitle
+      div.editor-preview-content
 </template>
 <script>
 export default {
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-
-    }
-}
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
-<style scoped>
+<style scoped lang="scss">
+$tools-bar-height: 45px;
+$border-color: #d9d9d9;
+$editor-font-color: #333;
+$editor-font-size: 16px;
+$editor-font-weight: 400;
+$editor-line-height: 30px;
+$editor-background-color: #fcfaf2;
+$editor-title-background-color: #f5f2f2;
+$editor-icon-color: gray;
+
 .editor-root {
   height: 100vh;
   display: flex;
   flex-direction: column;
-}
-.editor-tools-bar {
-  height: 30px;
-  flex: none;
-}
-.editor-container {
-  flex: auto;
-  display: flex;
-}
-.editor-input {
-  flex :auto;
-}
-.editor-preview {
-  flex: auto;
+  overflow: hidden;
+  .editor-container {
+    flex: auto;
+    overflow: hidden;
+    display: flex;
+    .editor-input {
+      flex-grow: 1;
+      flex-shrink: 0;
+      flex-basis: 50%;
+      border-right: 1px solid $border-color;
+      overflow: hidden;
+      .editor-tools-bar {
+        height: $tools-bar-height;
+        border-bottom: 1px solid $border-color;
+        display: flex;
+        .editor-tools-bar-title {
+          flex-grow: 1;
+          flex-shrink: 0;
+          flex-basis: 40%;
+          .editor-title-input {
+            width: 100%;
+            height: 100%;
+            padding: 0 10px 0;
+            border: none;
+            outline: none;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+          }
+        }
+        .editor-tools-bar-icons {
+          flex-grow: 1;
+          flex-shrink: 0;
+          flex-basis: 60%;
+          display: flex;
+          flex-wrap: nowrap;
+          justify-content: flex-end;
+          align-items: center;
+          i {
+            margin: 0 20px 0;
+            font-size: 20px;
+            color: $editor-icon-color;
+            text-align: center;
+          }
+        }
+      }
+      .editor-textarea {
+        height: calc(100vh - #{$tools-bar-height});
+        textarea {
+          display: block;
+          height: 100%;
+          width: 100%;
+          padding: 10px;
+          resize: none;
+          color: $editor-font-color;
+          background-color: $editor-background-color;
+          font-size: $editor-font-size;
+          font-weight: $editor-font-weight;
+          line-height: $editor-line-height;
+          border: none;
+          outline: none;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          overflow: auto;
+        }
+      }
+    }
+    .editor-preview {
+      flex-grow: 1;
+      flex-shrink: 0;
+      flex-basis: 50%;
+      overflow: auto;
+      display: flex;
+      flex-direction: column;
+      .editor-preview-title {
+        height: $tools-bar-height;
+        padding: 0 10px 0;
+        flex: none;
+        background-color: $editor-title-background-color;
+        border-bottom: 1px solid $border-color;
+        font-size: 22px;
+        text-align: left;
+        line-height: $tools-bar-height;
+      }
+      .editor-preview-content {
+        flex: auto;
+        padding: 10px;
+      }
+    }
+  }
 }
 </style>
