43f3015057c80e840e7cb37b29e8e8d1513c8fb1
GreatVincy <1358073296@qq.com>
2018-05-22 16:37:45 +0800

    加入全屏预览功能(IE11 and above)

--- src/components/notebook/NotebookViewer.vue
+++ src/components/notebook/NotebookViewer.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
-div.notebook-viewer(:style="{width: width, height: height, backgroundColor: backgroundColor}")
+div.notebook-viewer(:style="rootStyle")
   div.notebook-viewer-title-wrap
     div.notebook-viewer-title(class="text-ellipsis") {{title}}
     div.notebook-viewer-title-icons
-      i.fa.fa-expand(title="全屏预览")
+      i.fa(:class="fullscreen ? 'fa-compress' : 'fa-expand'" title="全屏预览" @click="toggleFullScreen")
   div.notebook-viewer-wrap 
     viewer(:raw="raw")    
 </template>
@@ -40,10 +40,76 @@ export default {
     viewer: Viewer
   },
 
+  mounted() {
+    window.onresize = () => {
+      this.fullscreen = this.isFullScreen();
+    };
+  },
+
   data() {
     return {
       fullscreen: false
     };
+  },
+  methods: {
+    toggleFullScreen() {
+      if (this.isFullScreen()) {
+        this.exitFullScreen();
+      } else {
+        this.requestFullScreen();
+      }
+    },
+    isFullScreen() {
+      return (
+        document.fullScreen ||
+        document.mozFullScreen ||
+        document.webkitIsFullScreen ||
+        document.msFullscreenElement
+      );
+    },
+    requestFullScreen() {
+      const el = this.$el;
+      const fn =
+        el.requestFullscreen ||
+        el.mozRequestFullScreen ||
+        el.webkitRequestFullscreen ||
+        el.msRequestFullscreen;
+      if (fn) {
+        el[fn.name || fn.toString().match(/function\s*([^\s(]+)/)[1]]();
+        this.fullscreen = true;
+      } else {
+        alert("当前浏览器不支持全屏！");
+      }
+    },
+    exitFullScreen() {
+      const fn =
+        document.exitFullscreen ||
+        document.mozCancelFullScreen ||
+        document.webkitExitFullscreen ||
+        document.msExitFullscreen;
+      fn &&
+        document[fn.name || fn.toString().match(/function\s*([^\s(]+)/)[1]]();
+      this.fullscreen = false;
+    }
+  },
+  computed: {
+    rootStyle() {
+      const style = {
+        backgroundColor: this.backgroundColor
+      };
+      if (this.fullscreen) {
+        style.position = "fixed";
+        style.width = "100vw";
+        style.height = "100vh";
+        style.top = "0";
+        style.left = "0";
+        style.zIndex = "999";
+      } else {
+        style.width = this.width;
+        style.height = this.height;
+      }
+      return style;
+    }
   }
 };
 </script>
